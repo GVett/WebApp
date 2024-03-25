256d1af51376e0109bf57072c4aa5475356d4b87
Garrett Vetter <garrettvetter@icloud.com>
2024-03-25 20:13:17 +0000

    Ethan page added

--- app/movie/routes.py
+++ app/movie/routes.py
@@ -1,12 +1,18 @@
 from flask import Blueprint, render_template, redirect
 from flask_login import login_required
-from replit import db
 from app.navigation import navigation
+import urllib.request
+import json
 
 movie_blueprint = Blueprint('movie_blueprint', __name__, template_folder = 'templates')
 
 @movie_blueprint.route('/movie')
 @login_required
 def movie():
-  # Render movie template (EPIC DA RATT MOMENTOOOOOOOOOOO)
-  return render_template("movie.html", navigation = navigation)
+  url = "https://api.themoviedb.org/3/discover/movie?api_key=47a3a40c4fe87e48606abbbc69c490c0"
+
+  response = urllib.request.urlopen(url)
+  data = response.read()
+  dict = json.loads(data)
+
+  return render_template ("movie.html", movies=dict["results"], navigation = navigation)
